ef3028191ff991fcc3746337f3f27ca318f53604
Sergio Gutierrez <nubesgladius20@gmail.com>
2020-08-15 20:42:19 -0700

    Code cleaning and removal
    - scroll.css removed as parallax effects are undesired now
    
    - blog.css code edited so that is is responsive on smartphones

--- js/navMenuMobile.js
+++ js/navMenuMobile.js
@@ -1,45 +1,55 @@
 //Mobile menu
-let closeNav = document.querySelector( ".close-nav" );
-let openNav = document.querySelector( ".open-nav" );
+let closeNav = document.querySelector(".close-nav");
+let openNav = document.querySelector(".open-nav");
 
-if ( window.matchMedia( '( max-device-width: 760px )' ).matches ) {
+if (window.matchMedia("( max-device-width: 760px )").matches) {
+  let header = document.querySelector(".header");
+  let headerItems = document.querySelectorAll(".header__item");
 
-    let header = document.querySelector(".header");
-    let headerItems = document.querySelectorAll( ".header__item" );
-    
-    document.addEventListener( "DOMContentLoaded", () => { 
-      //closeNav.classList.add( "hide" );  
-      headerItems.forEach( item => {
-        item.classList.add( "hide" );
+  document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+      //closeNav.classList.add( "hide" );
+      headerItems.forEach((item) => {
+        item.classList.add("hide");
       });
-      openNav.classList.remove( "hide" );
-      openNav.classList.add( "show" );
-    }, false);
-    
-    openNav.addEventListener( "click", () => {
-      header.style.height = "500px";
-      headerItems.forEach( item => {
-        item.classList.remove( "hide" );
-        item.classList.add( "show" );
-      });  
-      openNav.classList.remove( "show" );
-      openNav.classList.add( "hide" );
-    }, false);
-    
-    closeNav.addEventListener( "click", () => {
+      openNav.classList.remove("hide");
+      openNav.classList.add("show");
+    },
+    false
+  );
+
+  openNav.addEventListener(
+    "click",
+    () => {
+      header.style.height = "400px";
+      headerItems.forEach((item) => {
+        item.classList.remove("hide");
+        item.classList.add("show");
+      });
+      openNav.classList.remove("show");
+      openNav.classList.add("hide");
+    },
+    false
+  );
+
+  closeNav.addEventListener(
+    "click",
+    () => {
       header.style.height = "revert";
-      headerItems.forEach( item => {
-        item.classList.remove( "show" );
-        item.classList.add( "hide" );
+      headerItems.forEach((item) => {
+        item.classList.remove("show");
+        item.classList.add("hide");
       });
-      openNav.classList.remove( "hide" );
-      openNav.classList.add( "show" );
-    }, false);
+      openNav.classList.remove("hide");
+      openNav.classList.add("show");
+    },
+    false
+  );
 } else {
-  closeNav.classList.add( "hide" );  
-  openNav.classList.add( "hide" );
+  closeNav.classList.add("hide");
+  openNav.classList.add("hide");
 }
-
 
 //Update the current year on the footer copyright
 const footerYear = document.querySelector(".footer p:first-of-type");
